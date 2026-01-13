511e2d6c9a8a34be42fd0501f2e62ee621679140
mdazatorres <mdazatorres@cimat.mx>
2026-01-13 08:48:52 -0600

    I create a external function for the plot of the surge event.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from copy import deepcopy
 import datetime as dt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from func_to_plot import plot_occupancy,plot_ED_admissions
+from func_to_plot import plot_occupancy,plot_ED_admissions, plot_occupancy_under_scenarios
 from matplotlib.colors import TwoSlopeNorm
 # NOTE: Replace the following two lines by the actual import that gives you `df`, `fixed_params`, and the functions.
 from model_full_equilibrium_points import (
@@ -192,64 +192,10 @@ else : #mode=="Transient surge":
     # st.plotly_chart(fig, use_container_width=True)
 
     #########
-    from plotly.subplots import make_subplots
-    #def occupancy_under_scenarios
-    fig = make_subplots(
-        rows=3,
-        cols=1,
-        shared_xaxes=True,
-        specs=[[{"secondary_y": True}],
-               [{"secondary_y": True}],
-               [{"secondary_y": True}]]
-    )
+    fig_occ_under_scenarios = plot_occupancy_under_scenarios(x_ts, x0,t_cut, times_plot,x_ts_plot)
 
-    colors = ['blue', 'green', 'purple']
-    labels = ['Hs', 'Hm', 'ICU']
+    st.plotly_chart(fig_occ_under_scenarios , use_container_width=True)
 
-    extra_beds = x_ts - x0  # extra beds relative to equilibrium
-    extra_beds_plot= extra_beds[:t_cut]
-    for i in range(3):
-        # --- absolute occupancy (left axis) ---
-        fig.add_trace(go.Scatter(x=times_plot, y=x_ts_plot[:, i], mode="lines", name=f"{labels[i]} occupancy",
-                line=dict(color=colors[i])), row=i + 1, col=1,  secondary_y=False )
-
-        # equilibrium line
-        fig.add_hline( y=x0[i], line_dash="dot", line_color=colors[i],  row=i + 1, col=1 )
-
-        # --- extra beds (right axis, starts at 0) ---
-        fig.add_trace( go.Scatter(x=times_plot, y=extra_beds_plot[:, i], mode="lines",
-                name=f"{labels[i]} extra beds",line=dict(color=colors[i], dash="dash"), visible="legendonly",
-                showlegend=False ), row=i + 1,col=1, secondary_y=True)
-
-        # --- Extra beds (bars = discrete daily beds) ---
-        fig.add_trace(go.Bar( x=times_plot,
-                y=extra_beds_plot[:, i],
-                name=f"{labels[i]} extra beds (daily)",
-                marker_color=colors[i],
-                opacity=0.35,
-                showlegend=False
-            ),
-            row=i + 1, col=1, secondary_y=True
-        )
-
-        # right axis formatting
-        fig.update_yaxes( title_text="Extra beds", secondary_y=True,
-            row=i + 1, col=1,rangemode="tozero" )
-
-
-
-    # axis labels
-    fig.update_xaxes(title_text="Days", row=3, col=1)
-    fig.update_yaxes(title_text="Beds", secondary_y=False)
-    fig.update_layout(
-        title="Transient Bed Occupancy Under Surge Scenarios",
-        template="plotly_white",
-        height=600,
-        legend=dict(orientation="h", y=-0.15))
-
-    st.plotly_chart(fig, use_container_width=True)
-
-    ########
 
     # --------------------------------------------------------------------
     # Compute peak excess bed demand:
--- func_to_plot.py
+++ func_to_plot.py
@@ -6,6 +6,7 @@ import datetime as dt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+
 def plot_occupancy(df, baseline_eq, cap_IP_surge, cap_med_surg, cap_ICU, init_day_str, end_day_str):
 
     dates = df['Date']
@@ -68,4 +69,57 @@ def plot_ED_admissions(df, fixed_params, baseline_eq,init_day_str,end_day_str):
                   annotation_text=f"Equilibrium Admit ICU: {fixed_params['pED_ICU'] * fixed_params['gamma'] * baseline_eq['S']:.2f}",
                   row=3, col=1)
     fig.update_layout(height=800, showlegend=True, title_text=f"Observed ED Admissions Time Series (Data from {init_day_str} to {end_day_str})")
+    return fig
+
+def plot_occupancy_under_scenarios(x_ts, x0, t_cut, times_plot, x_ts_plot):
+    fig = make_subplots(
+        rows=3,
+        cols=1,
+        shared_xaxes=True,
+        specs=[[{"secondary_y": True}],
+               [{"secondary_y": True}],
+               [{"secondary_y": True}]] )
+
+    colors = ['blue', 'green', 'purple']
+    labels = ['Hs', 'Hm', 'ICU']
+
+    extra_beds = x_ts - x0  # extra beds relative to equilibrium
+    extra_beds_plot = extra_beds[:t_cut]
+    for i in range(3):
+        # --- absolute occupancy (left axis) ---
+        fig.add_trace(go.Scatter(x=times_plot, y=x_ts_plot[:, i], mode="lines", name=f"{labels[i]} occupancy",
+                                 line=dict(color=colors[i])), row=i + 1, col=1, secondary_y=False)
+
+        # equilibrium line
+        fig.add_hline(y=x0[i], line_dash="dot", line_color=colors[i], row=i + 1, col=1)
+
+        # --- extra beds (right axis, starts at 0) ---
+        fig.add_trace(go.Scatter(x=times_plot, y=extra_beds_plot[:, i], mode="lines",
+                                 name=f"{labels[i]} extra beds", line=dict(color=colors[i], dash="dash"),
+                                 visible="legendonly",
+                                 showlegend=False), row=i + 1, col=1, secondary_y=True)
+
+        # --- Extra beds (bars = discrete daily beds) ---
+        fig.add_trace(go.Bar(x=times_plot,
+                             y=extra_beds_plot[:, i],
+                             name=f"{labels[i]} extra beds (daily)",
+                             marker_color=colors[i],
+                             opacity=0.35,
+                             showlegend=False
+                             ),
+                      row=i + 1, col=1, secondary_y=True
+                      )
+
+        # right axis formatting
+        fig.update_yaxes(title_text="Extra beds", secondary_y=True,
+                         row=i + 1, col=1, rangemode="tozero")
+
+    # axis labels
+    fig.update_xaxes(title_text="Days", row=3, col=1)
+    fig.update_yaxes(title_text="Beds", secondary_y=False)
+    fig.update_layout(
+        title="Transient Bed Occupancy Under Surge Scenarios",
+        template="plotly_white",
+        height=600,
+        legend=dict(orientation="h", y=-0.15))
     return fig
